0e4db657dc455173a6456186d1be592f3d5acd46
Finn <finn.beiglboeck@empa.ch>
2025-05-08 10:01:04 +0000

    new changes to functions

--- mypackage/__init__.py
+++ mypackage/__init__.py
@@ -1,7 +1,11 @@
 from .geometry import Point, Line
 from .utils import distance
 
+
 def personal_message():
     print("Hello from John Doe!")
+
+
+__all__ = ["Point", "Line", "distance"]
 
 __version__ = "0.1.0"
